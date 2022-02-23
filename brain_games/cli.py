745abda9eabfdb05cac89a9eede1d04ee4d0c5d0
Alexey Tverskov <alexisatv@gmail.com>
2022-02-24 01:28:48 +0500

    fix: add app banner

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,12 @@
 import prompt
 
 
+def welcome_app():
+    print("Welcome to the Brain Games!")
+
+
 def welcome_user():
     username = prompt.string('May I have your name? ')
     print(f'Hello, {username}!')
+
+    return username
